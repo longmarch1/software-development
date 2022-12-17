41dfbdf330dda0c6629435299cabb79ad02574cc
longmarch1 <35318412+longmarch1@users.noreply.github.com>
2022-12-17 11:41:54 -0500

    sync

--- easy-problems/temp.ts
+++
@@ -1,6 +0,0 @@
-function getPascalTriangle(n) {
-    if (n === 1) return [[1]];
-    const result = getPascalTriangle(n - 1);
-    result.push(joinArray(result[n - 2]));
-    return result;
-}
--- leetcode/climbing-stairs.js
+++
@@ -1,9 +0,0 @@
-// You are climbing a staircase. It takes n steps to reach the top.
-
-// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
-
-const climbStairs = function (n) {
-    if (n === 2) return 2;
-    if (n === 1) return 1;
-    return climbStairs(n - 1) + climbStairs(n - 2);
-};
